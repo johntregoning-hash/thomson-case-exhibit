a2be8ef12d75b95300f6212ff4e3e840a94bb3f9
John Tregoning <john.tregoning@gmail.com>
2026-01-20 17:37:57 +0000

    Full MDX essays with proper routing and vindication fixes

--- dist/chunks/astro_eO78U9IG.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_BG5xGXoY.mjs';
-import 'clsx';
--- dist/chunks/diagnostic-delay_CUG_a0FK.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./diagnostic-delay_vWZUI9lG.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
